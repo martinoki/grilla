637b6ab34b9e2d4393b8cab9949296961f832731
Martin <mocchi@moorea.io>
2019-06-10 15:39:44 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Table from './containers/Table/Table'
+
+function App() {
+  return (
+    <Table />
+  );
+}
+
+export default App;
